d3aaf7df0cb20d3ce8ba3f6a873bb6fa7634f9ca
AFZL210 <tricktomade@gmail.com>
2024-09-17 23:23:36 +0530

    add job

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 import axios from "axios";
 
-const callApi = async () => {
-  axios.get("https://expp-ser.onrender.com/api/leaderboard").then((res) => {
-    console.log(res.data);
-  });
+const callApi = () => {
+    return axios.get("https://expp-ser.onrender.com/api/leaderboard")
+        .then((res) => {
+            console.log('1');
+        })
+        .catch((err) => {
+            console.log('err1');
+        });
+};
+
+const callChatApp = () => {
+    return axios.get('https://chat-app-rv46.onrender.com/')
+        .then((data) => {
+            console.log('2');
+        })
+        .catch((err) => {
+            console.log('err2');
+        });
 };
 
 setInterval(() => {
-  callApi();
+    Promise.all([callApi(), callChatApp()])
+        .then(() => {
+            console.log('done');
+        })
+        .catch((error) => {
+            console.log('error');
+        });
 }, 1200000);
